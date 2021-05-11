8287e158962738e62f783cb47015360695acac68
mirinaehi <garook@e-mirim.hs.kr>
2021-05-11 09:42:31 +0900

    주사위를 포지션에 적용

--- 보드게임.c
+++ 보드게임.c
@@ -22,10 +22,20 @@ void main(void)
 {
 	// 플레이어의 위치(0부터 시작)
 	int position = 0;
+	// 주사위
 	int dice;
 
 	srand(time(0));
-	dice = rand() % 6 + 1;
+	
+	printf("현재위치 : %d\n\n", position);
 
-	printf("%d\n", dice);
+	// 주사위는 1 ~ 6까지 나오게 설정
+	dice = rand() % 6 + 1;
+	printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
+	
+	// 던진 주사위값을 현재 위치에 적용
+	position += dice;
+	printf("현재위치 : %d\n\n", position);
+
+
 }
