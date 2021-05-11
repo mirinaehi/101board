fe43cc997fc2e0ac37db9b5baba841e031730a5b
mirinaehi <garook@e-mirim.hs.kr>
2021-05-11 09:25:24 +0900

    주사위 테스트

---
+++ 보드게임.c
@@ -0,0 +1,31 @@
+/*
+*	z : 549 507 6524
+*	보드게임
+*	총 40칸
+*	플레이의 속성
+*		1. 돈 : 2,000,000,000 시작
+*		2. 현재위치 : 0부터 시작
+		3. 황금열쇠
+			원하는 곳 갈수 있음
+			돈을 랜덤하게 준다( -5억 ~ +5억 )
+			TODO : 돈을뺏는다
+
+	주사위 1 ~ 6
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void main(void)
+{
+	// 플레이어의 위치(0부터 시작)
+	int position = 0;
+	int dice;
+
+	srand(time(0));
+	dice = rand() % 6 + 1;
+
+	printf("%d\n", dice);
+}
