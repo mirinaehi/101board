6cab78000bbc4ef4201dcccc422f526cba7e5eeb
mirinaehi <garook@e-mirim.hs.kr>
2021-05-31 13:55:18 +0900

    위치 59에 있을 때, 처음위치로 되돌아가는 기능

--- 보드게임.c
+++ 보드게임.c
@@ -46,10 +46,20 @@ void main(void)
 		position += dice;
 		printf("현재위치 : %d, 현재금액 : %lld \n\n", position, money);
 		
+		// TEST CODE
+		// position = 59;
+
 		if (position == 20)
 		{
 			printf("기부천사 - *위치 20*에 있을때 재산의 20%%를 사회에 기부합니다. \n");
 			money = (long long)(money * 0.8);
+			printf("현재위치 : %d, 현재금액 : %lld \n\n", position, money);
+		}
+
+		if (position == 59)
+		{
+			printf("위치 59에 있을때, 처음 지점으로 돌아갑니다.\n");
+			position = 0;
 			printf("현재위치 : %d, 현재금액 : %lld \n\n", position, money);
 		}
 
