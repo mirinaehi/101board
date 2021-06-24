a66c7623be421f179c2c129d6d7cba760f14dad7
mirinaehi <garook@e-mirim.hs.kr>
2021-06-24 14:49:07 +0900

    [문법설명] 2중 while을 2중 for문으로

---
+++ 07 - 03 구구단.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(void)
+{
+	for (int dan = 2; dan <= 9; dan++)
+	{		
+		for (int i = 1; i <= 9; i++)
+		{
+			printf("%d X %d = %d\n", dan, i, dan * i);
+		}
+		printf("\n");
+	}
+}
