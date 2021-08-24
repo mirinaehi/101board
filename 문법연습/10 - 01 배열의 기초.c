7465158bdf132a5d8a737f230ca48289e7218f5a
mirinaehi <garook@e-mirim.hs.kr>
2021-08-24 10:16:24 +0900

    요소접근을 반복문으로

--- 문법연습/10 - 01 배열의 기초.c
+++ 문법연습/10 - 01 배열의 기초.c
@@ -9,6 +9,8 @@ void main(void)
 	printf("%p\n", arr);
 
 	// 배열의 각 요소들을 출력
-	printf("%d %d %d %d", arr[0], arr[1], arr[2], arr[3]);
+	for (int i = 0; i < 4; i++) {
+		printf("%d ", arr[i]);
+	}
 
 }
