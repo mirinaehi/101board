0e11826271eb461610c49ba033d264fbc2cebfea
mirinaehi <garook@e-mirim.hs.kr>
2021-08-31 09:54:19 +0900

    평균추가

--- 성적처리.c
+++ 성적처리.c
@@ -1,28 +1,29 @@
 ﻿#include <stdio.h>
 
 void cal_grade(int score);
+float average(int a, int b, int c);
 
 void main(void)
 {
+	float avg;
+
 	int C_score;
 	printf("씨언어 점수를 입력하시오 : ");
 	scanf("%d", &C_score);
-	
 	cal_grade(C_score);
 	
-
 	int java_score;
 	printf("자바 점수를 입력하시오 : ");
 	scanf("%d", &java_score);
-	
 	cal_grade(java_score);
 	
-
 	int CA_score;
 	printf("컴퓨터구조 점수를 입력하시오 : ");
 	scanf("%d", &CA_score);
-
 	cal_grade(CA_score);
+
+	avg = average(C_score, java_score, CA_score);
+	printf("세 과목의 평균은 %f입니다\n", avg);
 }
 
 // 입력받은 점수(score)에 대한 등급을 출력
@@ -40,4 +41,10 @@ void cal_grade(int score)
 		printf("E");
 
 	printf("\n");
+}
+
+// 세 점수의 평균을 반환
+float average(int a, int b, int c)
+{
+	return (a + b + c) / 3.f;
 }
